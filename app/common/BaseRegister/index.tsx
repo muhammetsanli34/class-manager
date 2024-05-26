9391d19ea57d9c2f5bc77db3bacac3b80bb19c26
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-05-26 14:17:14 +0300

    Refactor BaseRegister component and add Header to layout

--- app/common/BaseRegister/index.tsx
+++ app/common/BaseRegister/index.tsx
@@ -1,6 +1,13 @@
 import Role from "@/types/Role";
 import style from "./style.module.scss";
-import { FormControl, Grid, Input } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  FormControl,
+  Grid,
+  Input,
+  Typography,
+} from "@mui/material";
 
 interface BaseRegisterProps {
   role: Role;
@@ -10,26 +17,33 @@ export default function BaseRegister(props: BaseRegisterProps) {
   return (
     <div className="container vertical-center">
       <FormControl className={style.register}>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <Input placeholder="First Name" />
-          </Grid>
-          <Grid item xs={6}>
-            <Input placeholder="Last Name" />
-          </Grid>
-          <Grid item xs={6}>
-            <Input placeholder="Email" />
-          </Grid>
-          <Grid item xs={6}>
-            <Input placeholder="Password" />
-          </Grid>
-          <Grid item xs={6}>
-            <Input type="" placeholder="Confirm Password" />
-          </Grid>
-          <Grid item xs={6}>
-            <Input type="file" placeholder="Profile Picture" />
-          </Grid>
-        </Grid>
+        <Typography variant="h4" align="center" style={{ marginTop: "5rem" }}>
+          Register
+        </Typography>
+        <Card>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Input placeholder="First Name" />
+              </Grid>
+              <Grid item xs={6}>
+                <Input placeholder="Last Name" />
+              </Grid>
+              <Grid item xs={6}>
+                <Input placeholder="Email" />
+              </Grid>
+              <Grid item xs={6}>
+                <Input placeholder="Password" />
+              </Grid>
+              <Grid item xs={6}>
+                <Input type="" placeholder="Confirm Password" />
+              </Grid>
+              <Grid item xs={6}>
+                <Input type="file" placeholder="Profile Picture" />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
       </FormControl>
     </div>
   );
