e37903d2abefe0b3b0b9109fb755bfe89fb68943
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-06-01 10:33:35 +0300

    type casting properly changed

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,11 +1,11 @@
 import prisma from "@/lib/prisma";
-import { User } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
 export async function POST(req: Request) {
     try {
-        const requestBody: User = await req.json();
+        const requestBody: Prisma.UserCreateInput = await req.json();
         const user = await prisma.user.create({
-            data: requestBody,
+            data: requestBody
         });
         return {
             status: 200,
