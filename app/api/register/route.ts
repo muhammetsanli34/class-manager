5dec6707fcfdff352569cfd6fc177dab8ea7c6b6
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-06-01 16:59:32 +0300

    Refactor POST route in register API

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,23 +1,32 @@
 import prisma from "@/lib/prisma";
+import { APIResponse } from "@/types/ApiResponse";
 import { Prisma } from "@prisma/client";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const requestBody: Prisma.UserCreateInput = await req.body;
-        requestBody.password_hash = await bcrypt.hash(requestBody.password_hash, 10);
-        const user = await prisma.user.create({
-            data: requestBody
-        });
-        return {
-            status: 200,
-            body: user,
-        };
-    } catch (error) {
-        return {
-            status: 400,
-            body: error,
-        };
-    }
+export async function POST(
+  req: NextApiRequest,
+  res: NextApiResponse<APIResponse<any>>
+) {
+  try {
+    const requestBody: Prisma.UserCreateInput = await req.body;
+    requestBody.password_hash = await bcrypt.hash(
+      requestBody.password_hash,
+      10
+    );
+    const user = await prisma.user.create({
+      data: requestBody,
+    });
+    return res.status(200).json({
+      status: 200,
+      data: user,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      data: error,
+      message: "Error creating user",
+    });
+  }
 }
