4f6fd2f312d4be5af43210ad111adf2f46a04e38
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-06-02 17:16:36 +0300

    used context pattern for error handling and setting form values

--- app/common/BaseRegister/index.tsx
+++ app/common/BaseRegister/index.tsx
@@ -1,22 +1,43 @@
-import Role from "@/types/Role";
+"use client";
+
 import style from "./style.module.scss";
 import {
+  Button,
   Card,
   CardContent,
-  FormControl,
   Grid,
   Input,
   Typography,
 } from "@mui/material";
+import FormBase from "@/components/form/FormBase";
+import BaseInput from "@/components/form/BaseInput";
+import registerSchema from "@/app/validations/Register";
+import { Role } from "@prisma/client";
 
 interface BaseRegisterProps {
   role: Role;
 }
 
 export default function BaseRegister(props: BaseRegisterProps) {
+  const formValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profilePicture: "",
+  };
+
   return (
     <div className="container vertical-center">
-      <FormControl className={style.register}>
+      <FormBase
+        className={style.register}
+        submit={() => {
+          console.log(registerSchema);
+        }}
+        values={formValues}
+        rules={registerSchema}
+      >
         <Typography variant="h4" align="center" style={{ marginTop: "5rem" }}>
           Register
         </Typography>
@@ -24,27 +45,45 @@ export default function BaseRegister(props: BaseRegisterProps) {
           <CardContent>
             <Grid container spacing={6}>
               <Grid item xs={6}>
-                <Input placeholder="First Name" />
+                <BaseInput placeholder="First Name" name="firstName" />
               </Grid>
               <Grid item xs={6}>
-                <Input placeholder="Last Name" />
+                <BaseInput placeholder="Last Name" name="lastName" />
               </Grid>
               <Grid item xs={6}>
-                <Input placeholder="Email" />
+                <BaseInput placeholder="Email" name="email" />
               </Grid>
               <Grid item xs={6}>
-                <Input placeholder="Password" />
+                <BaseInput placeholder="Password" name="password" />
               </Grid>
               <Grid item xs={6}>
-                <Input type="" placeholder="Confirm Password" />
+                <BaseInput
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                />
               </Grid>
               <Grid item xs={6}>
-                <Input type="file" placeholder="Profile Picture" />
+                <BaseInput
+                  type="file"
+                  placeholder="Profile Picture"
+                  name="profilePicture"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className="w-full"
+                  type="submit"
+                >
+                  Register
+                </Button>
               </Grid>
             </Grid>
           </CardContent>
         </Card>
-      </FormControl>
+      </FormBase>
     </div>
   );
 }
--- components/form/BaseInput/index.tsx
+++ components/form/BaseInput/index.tsx
@@ -1,21 +1,31 @@
 import { Input, InputProps } from "@mui/material";
+import { ErrorContext } from "../FormBase";
 
 interface BaseInputProps extends InputProps {
-  type: string;
-  placeholder: string;
-  className: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+  placeholder?: string;
+  className?: string;
+  value?: string;
 }
 
 export default function BaseInput(props: BaseInputProps) {
   return (
-    <Input
-      type={props.type}
-      placeholder={props.placeholder}
-      className={props.className}
-      value={props.value}
-      onChange={props.onChange}
-    />
+    <ErrorContext.Consumer>
+      {({ errors, values }) => (
+        <div className="flex flex-col">
+          <Input
+            {...props}
+            error={!!errors[props.name]}
+            onChange={(e) => {
+              console.log("values", values);
+              props.onChange
+                ? props.onChange(e)
+                : (values[props.name] = e.target.value);
+            }}
+          />
+          <span className="text-red-500">{errors[props.name]}</span>
+        </div>
+      )}
+    </ErrorContext.Consumer>
   );
 }
