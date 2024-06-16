cfc3090b6116757530159e00a4d398da0eb42d04
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-06-16 14:38:58 +0300

    Update token and user storage to use sessionStorage

--- app/common/BaseRegister/index.tsx
+++ app/common/BaseRegister/index.tsx
@@ -52,8 +52,8 @@ export default function BaseRegister(props: BaseRegisterProps) {
           if (res.status === 200) {
             alert(data.message, "success");
             router.push("/");
-            cookies().set("token", data.token);
-            cookies().set("user", JSON.stringify(data.user));
+            sessionStorage.setItem("token", data.token);
+            sessionStorage.setItem("user", JSON.stringify(data.user));
           } else {
             alert(data.message, "error");
           }
