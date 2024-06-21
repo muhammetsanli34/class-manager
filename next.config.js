bae6a3b1cd045b32f856099bff7f2e206456c09a
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-06-21 17:26:06 +0300

    Add redirect from root to home page

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true
     },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
